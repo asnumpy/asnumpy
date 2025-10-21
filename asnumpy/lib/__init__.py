465e043216c6eb51adf1c5028da04d8ca4fc1872
yang-jihan <1553170804@qq.com>
2025-10-22 03:33:47 +0800

    feat: add statistics and nn modules with mean and softmax APIs
    - Add statistics module with mean() function
    - Add nn module with softmax() activation function
    - Update CMakeLists and Python bindings
    - Add comprehensive test cases

--- asnumpy/lib/__init__.py
+++ asnumpy/lib/__init__.py
@@ -21,6 +21,8 @@ from .asnumpy_core.cann import *
 from .asnumpy_core.array import *
 from .asnumpy_core.logic import * 
 from .asnumpy_core.sorting import * 
+from .asnumpy_core.statistics import *
+from .asnumpy_core.nn import *
 from .asnumpy_core import linalg  
 # linalg模块内部分需要ap.linalg.xxx调用，部分ap.yyy调用，
 # yyy类函数分到了.asnumpy_core根模块中
@@ -164,7 +166,12 @@ __all__ = [
     "less_equal",
     "equal",
     "not_equal",
-    "sort"
+    # sorting
+    "sort",
+    # statistics
+    "mean",
+    # nn
+    "softmax",
 ]
 
 
