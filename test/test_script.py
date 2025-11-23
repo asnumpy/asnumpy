719a5c00ed328bc806bcfa0a264d946d00eebb75
gehaoxuan <2310314646@qq.com>
2025-11-23 14:32:05 +0800

    add extrema_finding module, move maximum, minimum, fmax, fmin to it, add max, amax, nanmax function

--- test/test_script.py
+++ test/test_script.py
@@ -108,6 +108,9 @@ MATH_FUNCTIONS = [
     ("log1p", np.log1p, ap.log1p, LOG_UNARY_TEST_CASES),
     ("logaddexp", np.logaddexp, ap.logaddexp, BINARY_TEST_CASES + BROADCAST_TEST_CASES),
     ("logaddexp2", np.logaddexp2, ap.logaddexp2, BINARY_TEST_CASES + BROADCAST_TEST_CASES),
+    ("max", np.max, ap.max, UNARY_TEST_CASES + PROD_DIM_TEST_CASES),
+    ("amax", np.amax, ap.amax, UNARY_TEST_CASES + PROD_DIM_TEST_CASES),
+    ("nanmax", np.nanmax, ap.nanmax, UNARY_TEST_CASES + NANPROD_DIM_TEST_CASES),
 ]
 
 LINALG_FUNCTIONS = [
@@ -155,7 +158,7 @@ FUNCTIONS_TABLE = MATH_FUNCTIONS + LINALG_FUNCTIONS + LOGIC_FUNCTIONS + SORTING_
 
 def test_functions():
     
-    for name, np_func, ap_func, test_cases in FUNCTIONS_TABLE:
+    for name, np_func, ap_func, test_cases in MATH_FUNCTIONS:
         print("=" * 50)
         print(f"Testing {name} function:")
         print("=" * 50)
@@ -174,10 +177,10 @@ def test_functions():
                         for arg in test_case
                     )
                     # === 特殊函数注释说明 ===
-                    # 测试 prod, sum, nanprod, nansum 时，可使用：np_func(test_case[0], axis=test_case[1], keepdims=test_case[2])
+                    # 测试 prod, sum, nanprod, nansum, max, amax 时，可使用：np_func(test_case[0], axis=test_case[1], keepdims=test_case[2])
                     # 测试 cross 时，可使用：np_func(test_case[0], test_case[1], axis=test_case[2])
                     # 测试 nan_to_num 时，可使用：np_func(test_case[0], nan=test_case[1], posinf=test_case[2], neginf=test_case[3])
-                    if name == "prod" or name == "sum" or name == "nanprod" or name == "nansum":
+                    if name == "prod" or name == "sum" or name == "nanprod" or name == "nansum" or name == "max" or name == "amax" or name == "nanmax":
                         np_result = np_func(test_case[0], axis=test_case[1], keepdims=test_case[2])
                     elif name == "cross":
                         np_result = np_func(test_case[0], test_case[1], axis=test_case[2])
