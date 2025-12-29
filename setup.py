3ae1206b7bc23c975aaf422d5e7e7d8eb7461f67
hyolee <lihongbo14@huawei.com>
2025-12-29 15:16:07 +0800

    test ci

--- setup.py
+++ setup.py
@@ -88,6 +88,7 @@ class CMakeBuild(build_ext):
         print(f"Building with CMake in {build_temp}...")
         subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=build_temp)
 
+
 npu_array_extension = Extension(
     name="asnumpy.lib.asnumpy_core",
     sources=[]
