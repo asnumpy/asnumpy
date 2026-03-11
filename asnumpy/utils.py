54b5905c6a719b85979a93a14dc1e4a14cb4d0bf
cann-robot <songchangxia@cann.team>
2026-03-11 11:34:54 +0800

    feat(logging): add loguru for frontend
    Co-authored-by: hyyisasleep<2106975800@qq.com>

--- asnumpy/utils.py
+++ asnumpy/utils.py
@@ -15,6 +15,7 @@
 # *****************************************************************************
 
 from typing import Sequence, Union, overload
+from loguru import logger
 import numpy as np
 from .lib.asnumpy_core import ndarray as _ndarray
 from .lib.asnumpy_core import broadcast_shape as _broadcast_shape
@@ -150,12 +151,16 @@ class ndarray(_ndarray):
         return super().to_numpy()
 
 
+@logger.catch
 def broadcast_shape(shape_a: Sequence[int], shape_b: Sequence[int]) -> tuple:
+    logger.debug(f"Broadcasting shapes {shape_a}, {shape_b}")
     return _broadcast_shape(shape_a, shape_b)
 
 
+@logger.catch
 def _convert_dtype(dtype):
     """Convert dtype parameter to appropriate format if needed"""
+    logger.debug(f"Converting dtype {dtype}")
     if dtype is None:
         return None
     if not isinstance(dtype, np.dtype):
@@ -163,8 +168,10 @@ def _convert_dtype(dtype):
     return dtype
 
 
+@logger.catch
 def _convert_size(size: Union[int, Sequence[int]]) -> Sequence[int]:
     """Convert size from int to tuple"""
+    logger.debug(f"Converting size {size}")
     if isinstance(size, int):
         return (size,)
     return size
