0f5c988aa2c24eb88ece13beeca6ab1d4f9adb3d
hyyisasleep <2106975800@qq.com>
2026-06-10 12:27:10 +0000

    fix bug

--- src/asnumpy/linalg/direct.py
+++ src/asnumpy/linalg/direct.py
@@ -27,17 +27,17 @@ from .._core import (
 )
 from .._fallback import fallback_to_numpy
 from .._types import ArrayLike
-from ..utils import as_host_array, ndarray
+from ..utils import as_host_array, ndarray, to_asnumpy_array
 
 
 def _requires_fp64_fallback(*arrays: ArrayLike) -> bool:
-    return any(np.asarray(_as_host_array(arr)).dtype == np.float64 for arr in arrays)
+    return any(np.asarray(as_host_array(arr)).dtype == np.float64 for arr in arrays)
 
 
 @fallback_to_numpy
 def dot(a: ArrayLike, b: ArrayLike) -> ndarray:
     if _requires_fp64_fallback(a, b):
-        return _to_asnumpy_array(np.dot(as_host_array(a), as_host_array(b)))
+        return to_asnumpy_array(np.dot(as_host_array(a), as_host_array(b)))
     return ndarray(_dot(a, b))
 
 
@@ -56,7 +56,7 @@ def outer(a: ArrayLike, b: ArrayLike) -> ndarray:
 @fallback_to_numpy
 def vdot(a: ArrayLike, b: ArrayLike) -> ndarray:
     if _requires_fp64_fallback(a, b):
-        return _to_asnumpy_array(np.vdot(as_host_array(a), as_host_array(b)))
+        return to_asnumpy_array(np.vdot(as_host_array(a), as_host_array(b)))
     return ndarray(_vdot(a, b))
 
 
